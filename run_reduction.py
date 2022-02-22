12a207058da8e8a306c8896f38d889fad09110aa
JeanBilheux <bilheuxjm@ornl.gov>
2022-02-22 12:28:08 -0500

    improved script to take absolute and relative path.

--- run_reduction.py
+++ run_reduction.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import glob
+import subprocess
 
 REDUCTION_SCRIPT = 'mcp_detector_correction.py --skipimg'
-DEBUGGING = True
+DEBUGGING = False
 
 
 def is_folder_contains_fits(folder_name):
@@ -16,20 +17,33 @@ def is_folder_contains_fits(folder_name):
 
 
 def build_output_path(folder_name):
+    folder_name = os.path.abspath(folder_name)
     path_parsed = folder_name.split("/")
-    facility = path_parsed[0]
-    instrument = path_parsed[1]
-    ipts = path_parsed[2]
+    facility = path_parsed[1]
+    instrument = path_parsed[2]
+
+    if instrument == 'snfs1':
+        index_offset = 2
+    else:
+        index_offset = 0
+
+    instrument = path_parsed[2+index_offset]
+    ipts = path_parsed[3+index_offset]
     shared = 'shared'
     autoreduce = 'autoreduce'
-    # images = path_parsed[3]
-    # mcp = path_parsed[4]
-    folder_structure_to_keep = os.path.sep.join(path_parsed[5:])
+    # images = path_parsed[4+index_offset]
+    # mcp = path_parsed[5+index_offset]
+    folder_structure_to_keep = os.path.sep.join(path_parsed[6+index_offset:])
+
+    print(f"facility: {facility}")
+    print(f"instrument: {instrument}")
+    print(f"ipts: {ipts}")
+    print(f"data folder: {folder_structure_to_keep}")
 
     if DEBUGGING:
         facility = '/Volumes/G-DRIVE/IPTS/'
 
-    base_output_folder = os.path.sep.join([facility, instrument, ipts, shared, autoreduce, folder_structure_to_keep])
+    base_output_folder = os.path.sep.join([os.path.sep + facility, instrument, ipts, shared, autoreduce, folder_structure_to_keep])
     return base_output_folder
 
 
@@ -44,19 +58,40 @@ def get_list_inside_folders(folder_name):
     return list_inside_folders
 
 
+
+def create_full_path(output_dir):
+    if os.path.exists(output_dir):
+        return
+    os.makedirs(output_dir)
+
+
+
 def run_reduction(list_folders=None):
     if list_folders is None:
         return
 
     for _folder in list_folders:
 
+        _folder = os.path.abspath(_folder)
+
         if is_folder_contains_fits(_folder):
 
             output_path = build_output_path(_folder)
+            print(f"creating output path: {output_path}") 
+            create_full_path(output_path)
+
             input_path = _folder
 
             cmd = f"{REDUCTION_SCRIPT} {input_path} {output_path}"
-            print(cmd)
+            print(f"Running: {cmd}")
+            
+            proc = subprocess.Popen(cmd, shell=True,
+                                    stdin=subprocess.PIPE,
+                                    universal_newlines=True,
+                                    cwd=output_path)
+            proc.communicate()
+            print(f"Done!")
+
             continue
 
         list_inside_folders = get_list_inside_folders(_folder)
@@ -65,7 +100,7 @@ def run_reduction(list_folders=None):
 
         run_reduction(list_folders=list_inside_folders)
 
-
+        
 
 
 
--- run_reduction.sh
+++ run_reduction.sh
@@ -4,5 +4,6 @@ if [ $1 == "--help" ] || [ $1 == "-h" ]
 then
   echo "Please provide at least one folder path to reduce!"
 else
-  python run_reduction.py $@
+   source /opt/anaconda/bin/activate /opt/anaconda/envs/ImagingReduction 
+   python /SNS/users/j35/git/manual_data_reduction/run_reduction.py $@
 fi
